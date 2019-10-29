953205df0ac9d73b2db5ee5c5bc4e19995a9c93f
Dmitry Fedyuk <dfediuk@gmail.com>
2019-10-29 22:05:07 +0600

    0.0.3

--- app/code/local/LesMills/YandexCheckout/Block/Form.php
+++ app/code/local/LesMills/YandexCheckout/Block/Form.php
@@ -3,12 +3,12 @@
  * 2019-10-29
  * A block class should be inherited from Mage_Core_Block_Abstract, otherwise Magento fails.
  */
-final class LesMills_YandexCheckout_Block_Form extends Mage_Core_Block_Abstract {
+final class LesMills_YandexCheckout_Block_Form extends Mage_Core_Block_Template {
 	/**
 	 * @override
-	 * @see Mage_Core_Block_Abstract::_toHtml()
-	 * @used-by Mage_Core_Block_Abstract::toHtml()
+	 * @see Mage_Core_Block_Abstract::getTemplate()
+	 * @used-by Mage_Core_Block_Template::getTemplateFile()
 	 * @return string
 	 */
-	protected function _toHtml() {return __METHOD__;}
+	function getTemplate() {return 'yandex_checkout/form.phtml';}
 }
