8fee4ed0bd6e96a32c216a5aa0e63867766bf596
Dmitry Fedyuk <dfediuk@gmail.com>
2019-11-11 13:09:03 +0600

    no message

--- app/code/community/Df/YandexCheckout/T/Case1.php
+++ app/code/community/Df/YandexCheckout/T/Case1.php
@@ -39,10 +39,42 @@ final class Case1 extends \Df\Core\T\Base {
 		$yc = new YC(); /** @var YC $yc */
 		$yc->setAuth('649593', 'test_GAYN1K-abG3t0cUwLRFuLdeLQXlz60SFVDqiuO4B_Eg');
 		$yp = $yc->createPayment([
+			/**
+			 * 2019-11-10
+			 * «Payment amount.
+			 * Sometimes Yandex.Checkout's partners charge additional commission from the users
+			 * that is not included in this amount.»
+			 * https://checkout.yandex.com/developers/api#payment_object_amount
+			 */
 			'amount' => ['currency' => 'RUB', 'value' => 100.0]
 			,'capture' => true
 			,'confirmation' => ['return_url' => 'https://www.merchant-website.com/return_url', 'type' => 'redirect']
+			/**
+			 * 2019-11-11
+			 * «Description of the transaction (maximum 128 characters)
+			 * displayed in your Yandex.Checkout Merchant Profile,
+			 * and shown to the user during checkout.»
+			 * https://checkout.yandex.com/developers/api#payment_object_description
+			 */
 			,'description' => 'Заказ №1'
+			/**
+			 * 2019-11-11
+			 * 1)
+			 *	,'recipient' => [
+			 *		'account_id' => ''
+			 *		,'gateway_id' => ''
+			 *	]
+			 * 2) Required. Object. «Payment recipient»
+			 * https://checkout.yandex.com/developers/api#payment_object_recipient
+			 * 3) Despite the documentation says that it is required, actually, it is not.
+			 * 4) The Yandex.Checkout library will fail if empty values are passed.
+			 * @see \YandexCheckout\Request\Payments\CreatePaymentRequestBuilder::setRecipient()
+			 * https://github.com/yandex-money/yandex-checkout-sdk-php/blob/1.5.4/lib/Request/Payments/CreatePaymentRequestBuilder.php#L120-L137
+			 * @see \YandexCheckout\Model\Recipient::setAccountId()
+			 * https://github.com/yandex-money/yandex-checkout-sdk-php/blob/1.5.4/lib/Model/Recipient.php#L67-L86
+			 * @see \YandexCheckout\Model\Recipient::setGatewayId()
+			 * https://github.com/yandex-money/yandex-checkout-sdk-php/blob/1.5.4/lib/Model/Recipient.php#L100-L121
+			 */
 		], uniqid('', true)); /** @var YP $yp */
 		echo df_json_encode($yp->jsonSerialize());
 	}
@@ -54,10 +86,44 @@ final class Case1 extends \Df\Core\T\Base {
 		$yc = new YC(); /** @var YC $yc */
 		$yc->setAuth('649593', 'test_GAYN1K-abG3t0cUwLRFuLdeLQXlz60SFVDqiuO4B_Eg');
 		$yp = $yc->createPayment([
+			/**
+			 * 2019-11-10
+			 * Required. Object.
+			 * «Payment amount.
+			 * Sometimes Yandex.Checkout's partners charge additional commission from the users
+			 * that is not included in this amount.»
+			 * https://checkout.yandex.com/developers/api#payment_object_amount
+			 */
 			'amount' => ['currency' => 'RUB', 'value' => 100.0]
 			,'capture' => true
 			,'confirmation' => ['return_url' => 'https://www.merchant-website.com/return_url', 'type' => 'redirect']
+			/**
+			 * 2019-11-11
+			 * Optional. String.
+			 * «Description of the transaction (maximum 128 characters)
+			 * displayed in your Yandex.Checkout Merchant Profile,
+			 * and shown to the user during checkout.»
+			 * https://checkout.yandex.com/developers/api#payment_object_description
+			 */
 			,'description' => 'Заказ №1'
+			/**
+			 * 2019-11-11
+			 * 1)
+			 *	,'recipient' => [
+			 *		'account_id' => ''
+			 *		,'gateway_id' => ''
+			 *	]
+			 * 2) Required. Object. «Payment recipient»
+			 * https://checkout.yandex.com/developers/api#payment_object_recipient
+			 * 3) Despite the documentation says that it is required, actually, it is not.
+			 * 4) The Yandex.Checkout library will fail if empty values are passed.
+			 * @see \YandexCheckout\Request\Payments\CreatePaymentRequestBuilder::setRecipient()
+			 * https://github.com/yandex-money/yandex-checkout-sdk-php/blob/1.5.4/lib/Request/Payments/CreatePaymentRequestBuilder.php#L120-L137
+			 * @see \YandexCheckout\Model\Recipient::setAccountId()
+			 * https://github.com/yandex-money/yandex-checkout-sdk-php/blob/1.5.4/lib/Model/Recipient.php#L67-L86
+			 * @see \YandexCheckout\Model\Recipient::setGatewayId()
+			 * https://github.com/yandex-money/yandex-checkout-sdk-php/blob/1.5.4/lib/Model/Recipient.php#L100-L121
+			 */
 		], uniqid('', true)); /** @var YP $yp */
 		$c = $yp->getConfirmation(); /** @var Confirmation $c */
 		echo $c->getConfirmationUrl();
